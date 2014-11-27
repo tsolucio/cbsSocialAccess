8d528b4953ca5f0227733cbda0275231ece52478
= <=>
2014-11-27 16:37:28 +0100

    Socila Access

---
+++ modules/cbsSocialAccess/language/en_us.lang.php
@@ -0,0 +1,27 @@
+<?php
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+
+$mod_strings = Array(
+    'cbsSocialAccess' => 'Social Access',
+    'cbsSocialAccess' => 'Social Access',
+    'socialaccessno' => 'Social Access No.',
+    'access_date' => 'Access Date',
+    'access_time' => 'Access Time',
+    'relid' => 'Related to',
+    'access_url' => 'Access URL',
+    'srvprovidor' => 'SRV Providor',
+    'socialaction' => 'Social action',
+
+    'LBL_CUSTOM_INFORMATION' => 'Custom Information',
+    'LBL_CBSSOCIALACCESS_INFORMATION' => 'Social Access Information',
+    'LBL_DESCRIPTION_INFORMATION' => 'Description',
+);
+
+?>
---
+++ modules/cbsSocialAccess/language/it_it.lang.php
@@ -0,0 +1,25 @@
+<?php
+$mod_strings = array (
+//label of the distributor module
+    'cbsSocialAccess' => 'Social Access',
+    'cbsSocialAccess' => 'Social Access',
+    'socialaccessno' => 'Social Access No.',
+    'access_date' => 'Accesso Data',
+    'access_time' => 'Accesso Tempo',
+    'relid' => 'Relato a',
+    'access_url' => 'Accesso URL',
+    'srvprovidor' => 'SRV Providor',
+    'socialaction' => 'Azione Sociale',
+    'Like' => 'Mi piace',
+    'Dislike' => 'Non mi piace piu',
+    'Email' => 'Email',
+    'Connect' => 'Connettere',
+    'Block' => 'Blocco',
+
+    'LBL_CUSTOM_INFORMATION' => 'Informazioni Personalizzate',
+    'LBL_CBSSOCIALACCESS_INFORMATION' => 'Social Access Information',
+    'LBL_DESCRIPTION_INFORMATION' => 'Informazioni Descrizione',
+ //end
+);
+
+?>
